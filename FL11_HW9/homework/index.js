33090be27e47b545910283e6f65ab1e5e8692c44
Dmytro Yatsyshyn <vovksamitnyk@gmail.com>
2019-07-16 22:43:30 +0300

    bugs fixed

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,3 +1,72 @@
+let people = [
+    {
+        "_id": "5b5e3168c6bf40f2c1235cd6",
+        "index": 0,
+        "birthday": '2016-03-18T00:00:00',
+        "eyeColor": "green",
+        "name": "Stein",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5b5e3168e328c0d72e4f27d8",
+        "index": 1,
+        "birthday": '1991-02-11T00:00:00',
+        "eyeColor": "blue",
+        "name": "Cortez",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5b5e3168cc79132b631c666a",
+        "index": 2,
+        "birthday": '1984-04-17T00:00:00',
+        "eyeColor": "blue",
+        "name": "Suzette",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5b5e31682093adcc6cd0dde5",
+        "index": 3,
+        "birthday": '1994-04-17T00:00:00',
+        "eyeColor": "green",
+        "name": "George",
+        "favoriteFruit": "banana"
+    }
+
+]
+
+/*============"(  00  )"=============*/
+
+function returnsNumbers(string) {
+    let array = [];
+    let numbers = "0123456789";
+    for (let i = 0; i < string.length; i++) {
+        for (let j = 0; j < numbers.length; j++) {
+            if (string[i] === numbers[j]) {
+                array.push(1 * string[i]);
+            }
+        }
+    }
+    return array;
+}
+
+returnsNumbers("n1um3ber95");
+
+
+/*============"(  01  )"=============*/
+
+function returnsTypes() {
+    let types = {};
+    for (let i = 0; i < arguments.length; i++) {
+        let typeOfElement = typeof arguments[i];
+        if (types[typeOfElement] === undefined) {
+            types[typeOfElement] = 1;
+            } else {
+                types[typeOfElement] += 1;
+            }
+    }
+    return types
+}
+returnsTypes(5, "ejfhvbej", null, alert, false, undefined, "ejfhvbej");
 
 
 /*============"(  02  )"=============*/
@@ -52,6 +121,38 @@ function showFormattedDate(notFormattedDate) {
 }
 showFormattedDate(new Date('2019-01-27T23:10:00'));
 
+
+//*============"(  06  )"=============*/
+
+function canConvertToDate(date) {
+    return new Date(date).getTime() > 0;
+}
+canConvertToDate('2016-13-18T00:00:00');
+
+
+//*============"(  07  )"=============*/
+
+const millisecInSec = 1000;
+const secInMin = 60;
+const minInHour = 60;
+const hourInDay = 24;
+
+function daysBetween(from, to) {
+    return Math.round((new Date(to).getTime() - new Date(from).getTime()) /
+                      (millisecInSec * secInMin * minInHour * hourInDay));
+}
+daysBetween(new Date('2016-03-18T00:00:00'), new Date('2016-04-19T00:00:00'))
+
+
+//*============"(  08  )"=============*/
+
+function getAmountOfAdultPeople(data) {
+    let amountOfAdultPeople = filterArray( data, function(x) {
+        return daysBetween(x.birthday, new Date()) > 18 * 365;
+    } );
+    return amountOfAdultPeople.length;
+}
+getAmountOfAdultPeople(people);
 
 
 //*============"(  09  )"=============*/
